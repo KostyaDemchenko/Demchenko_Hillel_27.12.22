ba654cff16dcaeee7ee01123442330774a6e7c51
KostyaDemchenko <kostya.doge@gmail.com>
2023-02-01 17:28:25 +0200

    HW-14_task3_fix-elements-arr

--- HW-14/task3/script.js
+++ HW-14/task3/script.js
@@ -1,14 +1,13 @@
-const columnArr = parseInt(prompt('Enter number of columns:'), 10);
-const rowsArr = parseInt(prompt('Enter number of rows:'), 10);
+const columnAmnt = parseInt(prompt('Enter number of columns:'), 10);
+const rowsAmnt = parseInt(prompt('Enter number of rows:'), 10);
 
-if (!Number.isNaN(columnArr) && !Number.isNaN(rowsArr)) {
+if (!Number.isNaN(columnAmnt) && !Number.isNaN(rowsAmnt)) {
   const array = [];
 
-  for (let i = 0; i < rowsArr; i++) {
+  for (let i = 0; i < rowsAmnt; i++) {
     array.push([]);
-    for (let j = 0; j < columnArr; j++) {
-      const num = j + i;
-
+    for (let j = 0; j < columnAmnt; j++) {
+      const num = parseInt(prompt(`Enter number for column ${j + 1} and row ${i + 1}:`), 10);
       if (!Number.isNaN(num)) {
         array[i].push(num);
       } else {
